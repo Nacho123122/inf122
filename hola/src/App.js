febb719851078c52ce19606791b2e2b43b1f2583
Nacho123122 <juanignaciojimenezcondori122@gmail.com>
2024-01-12 11:55:33 -0400

    fin dia 9

--- hola/src/App.js
+++ hola/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import './stylesheet/Evento.css';
+import Evento from './components/Evento.js'
+import EventoB from './components/EventoB.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Evento />
+      
+      <EventoB 
+        title="Py Pizza 03 - PyPuzzle"
+        image="des"
+        dateLiteral="09 de Diciembre de 2023 - 13:30 GMT-4"
+        placeLiteral="Mr Pizza, 20 de Octubre Frente"
+        description="¡Hola Pythonista!
+        Participa en este Evento Presencial de la comunidad Python La Paz
+        ¿Cuánto conoces de Python?
+        Aprende a trabajar en equipo resolviendo increíbles acertijo
+        ¡Anímate a ser parte de esta actividad!" 
+        holi="holi"
+        
+        />
+      
     </div>
   );
 }
+
 
 export default App;
